0480f63b969dd53de5c75162ab36cc5584860403
chair1842 <207965457+chair1842@users.noreply.github.com>
2025-11-12 16:28:21 +0800

    remove whitespace

--- src/entities/player.cpp
+++ src/entities/player.cpp
@@ -23,7 +23,6 @@ void Player::update(float dt, vector<unique_ptr<Entity>>& entity_list) {
 
 	// clamp the position to bounds
 	position = Vector2f(clamp<float>(position.x, 0, 480 - rect_size.x), clamp<float>(position.y, 0, 480 - rect_size.y)); // rect.y isnt nessecary, but anyways
-
 }
 
 Player::Player(float size, Vector2f pos) 
